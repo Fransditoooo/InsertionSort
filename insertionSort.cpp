cc74bb21bb2954ac94f259292135c008379a6d63
Fransditoooo <fransdito.bayu.ft24@mail.umy.ac.id>
2025-03-24 03:38:23 +0700

    Membuat jarak per baris program

--- insertionSort.cpp
+++ insertionSort.cpp
@@ -11,7 +11,7 @@ while (true)
 cout << "Masukkan Jumlah Data pada Array : ";
 cin >> n;
 
-if (n <== 20)
+if (n <= 20)
 { // Membuat Kondisi n tidak lebih dari 20
 break;
 }
@@ -20,5 +20,9 @@ else
 cout << "\nArray yang anda masukkan maksimal 20 Elemen. \\n";
 }
 }
+cout << endl; // Membuat jarak per baris program
+cout << "===============" << endl; // Membuat tampilan susunan data element array
+cout << "Masukkan Elemen Array" << endl;
+cout << "==============="
 
-}
+for (int i = 0; i < n; i++) // Menggunakan perulangan for untuk menyimpan data pada array
