e173cafd351608631e693426d5bad095d348192c
Fransditoooo <fransdito.bayu.ft24@mail.umy.ac.id>
2025-03-24 03:25:31 +0700

    asukkan Jumlah Data pada Array

---
+++ insertionSort.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int arr[20]; // Membuat Array dengan panjang data 20
+int n; // Membuat Variabel inputan n
+
+void input ()
+{ //procedure Input
+while (true)
+{
+cout << "Masukkan Jumlah Data pada Array : ";
+cin >> n;
+
+if (n <== 20)
+{ // Membuat Kondisi n tidak lebih dari 20
+break;
+}
+else
+{
+
+}
+}
+
+}
